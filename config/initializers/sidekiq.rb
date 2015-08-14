d7a55c0a8c47cee6ca720240e8fe8a1c00500e9b
ryonext <ryonext.s@gmail.com>
2015-08-14 22:13:57 +0900

    Use RedisToGo

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
   if Rails.env.production?
-    if ENV['REDISCLOUD_URL']
-      config.redis = { url: ENV['REDISCLOUD_URL'], namespace: 'sidekiq' }
+    if ENV['REDISTOGO_URL']
+      config.redis = { url: ENV['REDISTOGO_URL'], namespace: 'sidekiq' }
     end
   else
     config.redis = { url: 'redis://localhost:6379', namespace: 'sidekiq' }
